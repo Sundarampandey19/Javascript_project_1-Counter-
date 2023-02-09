de3c676de57c341122763707e48ddf0a0b946808
Sundaram pandey <thisnewcomputerismine2020@gmail.com>
2023-02-09 21:37:35 +0530

    fixed the js bug

--- codes/script.js
+++ codes/script.js
@@ -1,35 +1,58 @@
-(function(){
-    const buttons = document.querySelectorAll('cont3')
-    let count = 0
+ (function(){
+     const buttons = document.querySelectorAll('button');
+     let count = 0;
 
 
-    buttons.forEach(function(button) {
-        addEventListener('click', function(){
-
+     buttons.forEach(function(button) {
+         button.addEventListener('click', function() {
             if(button.classList.contains('button-x')){
-                count--
+                count--;
+            } else if(button.classList.contains('button-y')){
+                count++;
             }
-            else if(button.classList.contains('button-y')){
-                count++
-            }
+             
+            const counter = document.querySelector('#cont2');
+            counter.textContent = count;
+             
 
-            const counter = querySelector('cont2')
-            counter.textContent = count
-
-            if(count<0){
-                counter.style.color = 'red'
-            }
-            else if(count>0){
-                counter.style.color = 'green'
-            }
-            else if(count == 0){
-                counter.style.color = 'black'
+            if(count<0) {
+                counter.style.color = 'red';
+            } else if(count>0){
+                 counter.style.color = 'green';
+            } else if(count == 0){
+                 counter.style.color = 'black';
             } 
-
-
-        })
+           }); 
+          });  
+        })();
+        
     
-    })
+     
 
-})()
+ 
 
+// (function () {
+//     const buttons = document.querySelectorAll('button');
+//     let count = 0;
+  
+//     buttons.forEach(function (button) {
+//       button.addEventListener('click', function () {
+//         if (button.classList.contains('button-x')) {
+//           count--;
+//         } else if (button.classList.contains('button-y')) {
+//           count++;
+//         }
+  
+//         const counter = document.querySelector('#cont2');
+//         counter.textContent = count;
+  
+//         if (count < 0) {
+//           counter.style.color = 'red';
+//         } else if (count > 0) {
+//           counter.style.color = 'green';
+//         } else if (count == 0) {
+//           counter.style.color = 'black';
+//         }
+//       });
+//     });
+//   })();
